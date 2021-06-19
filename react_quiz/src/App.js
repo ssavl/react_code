3f3ccd7027f30dadd51896223c743f02b928b5ed
ssavl <get-moscow@yandex.ru>
2021-06-19 20:09:44 +0300

    добавил в проект новое приложение - Quiz

---
+++ react_quiz/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Layout from './hoc/Layout'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>new React app</h1>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
